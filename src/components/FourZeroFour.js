61b6ec2181978ecdcf95d2872e8162ce2c1f971e
occiandiaali <ocean.diaali@gmail.com>
2021-07-28 20:03:51 +0100

    add logo

--- src/components/FourZeroFour.js
+++ src/components/FourZeroFour.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, Image, View} from 'react-native';
 
 export default function FourZeroFour() {
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.hoodie}
+        source={require('../res/images/hacker.png')}
+      />
       <Text style={styles.heading}>Ooops!</Text>
-      <Text style={styles.sub}>Looks like there's nothing here...</Text>
+      <Text style={styles.sub}>Sorry, I couldn't summon this story...</Text>
+      <Text style={styles.sub}>
+        Just tap the back button above to see more stories...
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 150,
+    marginVertical: 90,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 12,
@@ -23,8 +30,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontStyle: 'italic',
   },
+  hoodie: {
+    width: 150,
+    height: 150,
+  },
   sub: {
     color: '#ff0000',
-    fontSize: 27,
+    fontSize: 25,
+    padding: 5,
   },
 });
